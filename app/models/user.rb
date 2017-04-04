862fb960aaf5fe21e9b88e2802fe9fad699db592
Trista Townsend <tristatownsend@gmail.com>
2017-04-04 13:52:34 -0700

    add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
   validates(:first_name, { presence: true })
-  validates(:last_name, { presence: true })
-  validates(:email, { presence: true })
+validates(:last_name, { presence: true })
+validates(:email, { presence: true, uniqueness: true })
 
-  def self.search(search_term)
-    where(['first_name LIKE ? OR last_name LIKE ? OR email LIKE ?', "#{search_term}", "#{search_term}", "#{search_term}"])
-  end
+def self.search(search_term)
+  where(['first_name ILIKE ? OR last_name ILIKE ? OR email ILIKE ?', "%#{search_term}%", "%#{search_term}%", "%#{search_term}%"])
+end
+
+def self.created_after(date)
+  where(['created_at > ?', "#{date}"])
+end
+
+def self.is_not(name)
+  where('first_name != ? AND last_name != ?', "#{name}", "#{name}")
+end
 end
