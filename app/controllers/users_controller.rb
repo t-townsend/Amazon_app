603de63e985dbf961830e3e19c9235c6ed488e39
Trista Townsend <tristatownsend@gmail.com>
2017-04-04 15:01:26 -0700

    complete user sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      # session[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect_to products_path
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ end
 
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+
+  end
+
   root 'welcome#index'
 
 end
